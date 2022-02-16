18ec48f427a2d5a2c65d328a49b3e26c6831c4da
patryCCio <patryk.szczerbinski@o2.pl>
2022-02-16 12:42:04 +0100

    Nowa array lista typu Character przechowująca znaki zmienna helper, bufferedreader oraz gettey i settery

--- src/logic/CalcLogic.java
+++ src/logic/CalcLogic.java
@@ -1,12 +1,41 @@
 package logic;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 
 public class CalcLogic {
 
     private ArrayList<Double> actions = new ArrayList<>();
+    private ArrayList<Character> characters = new ArrayList<>();
 
+    private String helper;
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    public ArrayList<Character> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(ArrayList<Character> characters) {
+        this.characters = characters;
+    }
+
+    public String getHelper() {
+        return helper;
+    }
+
+    public void setHelper(String helper) {
+        this.helper = helper;
+    }
+
+    public BufferedReader getBr() {
+        return br;
+    }
+
+    public void setBr(BufferedReader br) {
+        this.br = br;
+    }
 
     public ArrayList<Double> getActions() {
         return actions;
