1417f9c03e1e19f892ebb2e11fe5591497213ae2
patryCCio <patryk.szczerbinski@o2.pl>
2022-02-16 12:30:46 +0100

    Tworze  nowa liste ctions, ktora bedzie przechowywała dzialania

--- src/logic/CalcLogic.java
+++ src/logic/CalcLogic.java
@@ -1,4 +1,18 @@
 package logic;
 
+import java.util.ArrayList;
+
 public class CalcLogic {
+
+    private ArrayList<Double> actions = new ArrayList<>();
+
+
+
+    public ArrayList<Double> getActions() {
+        return actions;
+    }
+
+    public void setActions(ArrayList<Double> actions) {
+        this.actions = actions;
+    }
 }
