41ea9ec1e6d2089f080bd9e6893974a0d787137c
patryCCio <patryk.szczerbinski@o2.pl>
2022-02-17 14:42:21 +0100

    git status

--- src/app/MainApp.java
+++ src/app/MainApp.java
@@ -146,5 +146,6 @@ public class MainApp {
         }
 
         new CalcController(calcLogic);
+
     }
 }
--- src/controller/CalcController.java
+++ src/controller/CalcController.java
@@ -11,6 +11,13 @@ public class CalcController {
     }
 
     private void checkAction() {
+        calcLogic.setMultiply(false);
+        calcLogic.setBracket(false);
+
+        for(int x=0; x<calcLogic.stringBuilder.length(); x++){
+            if(calcLogic.stringBuilder.charAt(x)=='(')calcLogic.setBracket(true);
+            if(calcLogic.stringBuilder.charAt(x)=='*'||calcLogic.stringBuilder.charAt(x)=='/')calcLogic.setMultiply(true);
+        }
         if(calcLogic.getLeftBracket()>0)calcLogic.setBracket(true);
         System.out.println("Wszystko poszło zgodnie z planem!");
         System.out.println("Działanie otrzymane z MainApp: " + calcLogic.stringBuilder);
