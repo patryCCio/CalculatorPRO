c7e0da6797f3eca5dbcd8aa14ec577cbabca317b
patryCCio <patryk.szczerbinski@o2.pl>
2022-02-20 21:26:29 +0100

    Wszystkie klasy zaaktualizowane pora się wziąć za testy

--- src/controller/CalcController.java
+++ src/controller/CalcController.java
@@ -12,7 +12,6 @@ public class CalcController{
         return result;
     }
 
-    //v
     public void doAction(CalcData calcData) {
         calcData.helper = "";
         checkAction(calcData);
@@ -175,7 +174,6 @@ public class CalcController{
 
     }
 
-
     public static void createArray(CalcData calcData, StringBuilder sb){
         calcData.helper = "";
 
@@ -239,119 +237,7 @@ public class CalcController{
     public static void printResult(CalcData calcData){
         System.out.println(calcData.toString() + "\n");
     }
-    //^
 
-    public static void createActionMultiply(CalcData calcData, int start, int end){
-        int x=0;
-
-        while(calcData.stringHelper.charAt(x)!='*' && calcData.stringHelper.charAt(x)!='/'){
-            x++;
-        }
-
-        while(x>=0 && calcData.stringHelper.charAt(x) != '+' && calcData.stringHelper.charAt(x) != '-'){
-            x--;
-        }
-//3-(-3*2+3)
-        if(x!=0) {
-            start = x;
-            x++;
-        }
-        else if (calcData.stringHelper.charAt(0)=='-'){
-            start = x;
-            calcData.stringSpecialHelper.append(calcData.stringHelper.charAt(x));
-            x++;
-        }else{
-            start = x;
-        }
-
-
-        do{
-            calcData.stringSpecialHelper.append(calcData.stringHelper.charAt(x));
-            x++;
-        }while(x < calcData.stringHelper.length() && calcData.stringHelper.charAt(x)!='+' && calcData.stringHelper.charAt(x) != '-');
-        end = x;
-
-        System.out.println("String special helper: " + calcData.stringSpecialHelper);
-        createMultiplyAction(calcData, start, end);
-    }
-
-    public static void createMultiplyAction(CalcData calcData, int start, int end) {
-        calcData.helper = "";
-        int startSpecial=0;
-        int endSpecial=0;
-
-        if(calcData.stringSpecialHelper.charAt(0)!='-') calcData.stringSpecialHelper.replace(0, 0, "+");
-        for(int x = 0; x< calcData.stringSpecialHelper.length(); x++){
-            if(calcData.stringSpecialHelper.charAt(x)=='/'|| calcData.stringSpecialHelper.charAt(x)=='+'|| calcData.stringSpecialHelper.charAt(x)=='-'|| calcData.stringSpecialHelper.charAt(x)=='*'){
-                calcData.characters.add(calcData.stringSpecialHelper.charAt(x));
-            }else{
-                do{
-                    calcData.helper = calcData.helper + calcData.stringSpecialHelper.charAt(x);
-                    x++;
-                }while(x< calcData.stringSpecialHelper.length() && calcData.stringSpecialHelper.charAt(x) != '*' && calcData.stringSpecialHelper.charAt(x) != '/');
-                calcData.actions.add(Double.valueOf(calcData.helper));
-                calcData.helper = "";
-                x--;
-            }
-        }
-        if(calcData.stringSpecialHelper.charAt(0)=='+'){
-            calcData.stringSpecialHelper.delete(0, 1);
-            int length = calcData.stringSpecialHelper.length();
-            startSpecial = calcData.stringBuilder.indexOf(String.valueOf(calcData.stringSpecialHelper));
-            endSpecial = startSpecial + length;
-            startSpecial--;
-            start--;
-            calcData.stringSpecialHelper.replace(0, 0, "+");
-        }else{
-            int length = calcData.stringSpecialHelper.length();
-            startSpecial = calcData.stringBuilder.indexOf(String.valueOf(calcData.stringSpecialHelper));
-            endSpecial = startSpecial + length;
-        }
-
-        calcData.stringSpecialHelper.delete(0, calcData.stringSpecialHelper.length());
-        double result = CalcController.getResult(calcData);
-        if(result>=0){
-            calcData.stringBuilder.replace(startSpecial+1, endSpecial, String.valueOf(result));
-            calcData.stringHelper.replace(start+1, end, String.valueOf(result));
-        }else{
-            calcData.stringBuilder.replace(startSpecial, endSpecial, String.valueOf(result));
-            calcData.stringHelper.replace(start, end, String.valueOf(result));
-        }
-        checkStringHelper(calcData);
-    }
-
-
-    public static void createArrayStringHelper(CalcData calcData){
-        calcData.helper = "";
-        if(calcData.stringHelper.charAt(0)!='-' && calcData.stringHelper.charAt(0) != '+'){
-            calcData.stringHelper.replace(0, 0, "+");
-        }
-
-        for(int x=0; x<calcData.stringHelper.length(); x++){
-            if(calcData.stringHelper.charAt(x) == '+' || calcData.stringHelper.charAt(x) == '-' || calcData.stringHelper.charAt(x) == '*' || calcData.stringHelper.charAt(x) == '/'){
-                calcData.characters.add(calcData.stringHelper.charAt(x));
-            }else{
-                do{
-                    calcData.helper = calcData.helper + calcData.stringHelper.charAt(x);
-                    x++;
-                }while(x < calcData.stringHelper.length() && calcData.stringHelper.charAt(x) != '-' && calcData.stringHelper.charAt(x) != '+' && calcData.stringHelper.charAt(x) != '*' && calcData.stringHelper.charAt(x) != '/');
-                calcData.actions.add(Double.valueOf(calcData.helper));
-                calcData.helper = "";
-                x--;
-            }
-        }
-
-        calcData.stringHelper.delete(0, calcData.stringHelper.length());
-    }
-
-    public static void checkStringHelper(CalcData calcData){
-        calcData.setMultiply(false);
-        for(int x=0; x<calcData.stringHelper.length(); x++){
-            if(calcData.stringHelper.charAt(x) == '*' || calcData.stringHelper.charAt(x) == '/') calcData.setMultiply(true);
-        }
-    }
-
-    //ważne V
     public static void checkAction(CalcData calcData){
         CalcData.howSpecialBracket = 0;
         calcData.setLeftBracket(0);
--- src/controller/SpecialBracketController.java
+++ src/controller/SpecialBracketController.java
@@ -4,99 +4,54 @@ import data.CalcData;
 
 public class SpecialBracketController implements CalcOperator {
 
-    CalcData calcData;
-    int actualSpecialBracket = 0;
-    int start, end;
-    double result;
-
     @Override
     public void createAction(CalcData calcData) {
         do {
             int where = CalcController.checkWhereSpecial(calcData, calcData.stringBuilder);
             CalcController.createSpecialBracket(calcData, calcData.specialBracket, where);
             CalcController.checkStartEnd(calcData, calcData.specialBracket);
+            reverseAction(calcData, calcData.specialBracket);
+
+            calcData.actions.clear();
+            calcData.characters.clear();
+
+            CalcController.createArray(calcData, calcData.specialBracket);
             double result = CalcController.getResult(calcData);
-            reverseAction(calcData, calcData.specialBracket, result);
-
-            calcData.start = calcData.start - 1;
-            calcData.end = calcData.end + 1;
-
             CalcController.saveResult(calcData, result);
             CalcController.checkAction(calcData);
+            CalcController.deleteBuilder(calcData.specialBracket);
 
-            CalcController.deleteBuilder(calcData.bracket);
-
-        } while (calcData.isBracket());
+        } while (calcData.isSpecialBracket());
     }
 
 
     //2*(-3+2/3) 2*(-3+23+2) 2*(-3+23+2)+(-3+23-5)
 
-    private void reverseAction(CalcData calcData, StringBuilder sb, double result){
-        sb.append(calcData.stringBuilder.charAt(calcData.start-2));
+    private void reverseAction(CalcData calcData, StringBuilder sb){
 
-        calcData.start--;
+        calcData.start = calcData.start - 2;
+        sb.append(calcData.stringBuilder.charAt(calcData.start));
+
 
         while(calcData.start >= 0 && calcData.stringBuilder.charAt(calcData.start) != '(' && calcData.stringBuilder.charAt(calcData.start) != '-' && calcData.stringBuilder.charAt(calcData.start) != '+'){
-
+            calcData.start--;
         }
 
+        calcData.start++;
+        int x = calcData.start;
+
+        while(calcData.stringBuilder.charAt(x)!='('){
+            sb.append(calcData.stringBuilder.charAt(x));
+            x++;
+        }
+        sb.delete(sb.length()-1, sb.length());
+
+        calcData.end++;
     }
 
     //3+3*(-3*2)
     //3*(-2*3+3) nie dziala
 
-    private void createActionMultiply(double result, int start, int end) {
-        calcData.helper = "";
-        calcData.stringHelper.append(result);
-        calcData.stringHelper.append(calcData.stringBuilder.charAt(start-1));
-        start--;
-        while(start >= 0 && calcData.stringBuilder.charAt(start) != '(' && calcData.stringBuilder.charAt(start) != '-' && calcData.stringBuilder.charAt(start) != '+'){
-            start--;
-        }
-        start++;
-        int x = start;
-        while(calcData.stringBuilder.charAt(x)!= '('){
-            calcData.stringHelper.append(calcData.stringBuilder.charAt(x));
-            x++;
-        }
-        calcData.stringHelper.delete(calcData.stringHelper.length()-1, calcData.stringHelper.length());
-
-        CalcController.createArrayStringHelper(calcData);
-        result = CalcController.getResult(calcData);
-
-        if(result>=0){
-            calcData.stringBuilder.replace(start, end, String.valueOf(result));
-        }else{
-            if(start==0){
-                calcData.stringBuilder.replace(start, end, String.valueOf(result));
-            }else{
-                if(calcData.stringBuilder.charAt(start-1)=='+'){
-                    calcData.stringBuilder.replace(start-1, end, String.valueOf(result));
-                }else{
-                    result -= 2*result;
-                    calcData.stringBuilder.replace(start-1, end, "+" + result);
-                }
-            }
-
-        }
-
-    }
-
-    private void specialBracket(int x) {
-        if (calcData.stringBuilder.charAt(x) == '-' && calcData.stringBuilder.charAt(x - 1) == '(' && calcData.stringBuilder.charAt(x - 2) == '*') {
-            ++actualSpecialBracket;
-        }
-        if (calcData.stringBuilder.charAt(x) == '-' && calcData.stringBuilder.charAt(x - 1) == '(' && calcData.stringBuilder.charAt(x - 2) == '/') {
-            ++actualSpecialBracket;
-        }
-        if (calcData.stringBuilder.charAt(x) == '-' && calcData.stringBuilder.charAt(x - 1) == '(' && calcData.stringBuilder.charAt(x - 2) == '-') {
-            ++actualSpecialBracket;
-        }
-        if (calcData.stringBuilder.charAt(x) == '-' && calcData.stringBuilder.charAt(x - 1) == '(' && calcData.stringBuilder.charAt(x - 2) == '+') {
-            ++actualSpecialBracket;
-        }
-    }
 
     @Override
     public void repairAction(double result) {
